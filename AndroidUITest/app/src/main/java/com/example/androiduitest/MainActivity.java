0c7a1bf4e2841ccb27ff4d19e4fb02f95459d7b6
MemoryReload <heping_tsdwx@163.com>
2018-05-07 23:37:37 +0800

    add Dialog example code.

--- AndroidUITest/app/src/main/java/com/example/androiduitest/MainActivity.java
+++ AndroidUITest/app/src/main/java/com/example/androiduitest/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.androiduitest;
 
 import android.annotation.SuppressLint;
+import android.content.DialogInterface;
 import android.graphics.drawable.Drawable;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -33,15 +35,33 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.activity_main_button:
-                if (mResource == R.drawable.dog_lord){
-                    mResource = R.drawable.dog_and_cat;
-                }
-                else {
-                    mResource = R.drawable.dog_lord;
-                }
-                imageView.setImageResource(mResource);
-                String inputText = editText.getText().toString();
-                Toast.makeText(this,inputText,Toast.LENGTH_SHORT).show();
+//                if (mResource == R.drawable.dog_lord){
+//                    mResource = R.drawable.dog_and_cat;
+//                }
+//                else {
+//                    mResource = R.drawable.dog_lord;
+//                }
+//                imageView.setImageResource(mResource);
+
+//                String inputText = editText.getText().toString();
+//                Toast.makeText(this,inputText,Toast.LENGTH_SHORT).show();
+                AlertDialog.Builder dialog = new AlertDialog.Builder(v.getContext());
+                dialog.setTitle("This is a Dialog.");
+                dialog.setMessage("Something important.");
+                dialog.setCancelable(false);
+                dialog.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+                dialog.setNegativeButton("Cancle", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+                dialog.show();
                 break;
                 default:
                     break;
