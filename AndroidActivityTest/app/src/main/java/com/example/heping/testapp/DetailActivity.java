b56bc16421469a052c4827ce64fb1eb7a2bbddf9
MemoryReload <heping_tsdwx@163.com>
2018-05-13 21:35:09 +0800

    add four activity launch modes example

--- AndroidActivityTest/app/src/main/java/com/example/heping/testapp/DetailActivity.java
+++ AndroidActivityTest/app/src/main/java/com/example/heping/testapp/DetailActivity.java
@@ -3,11 +3,12 @@ package com.example.heping.testapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class DetailActivity extends AppCompatActivity {
 
-    static final String ACTION_DETAIL="com.example.heping.testapp.start_detail";
     static final int RESULTCODE_DETAIL=54321;
     static final String PARAM_KEY="from";
     static final String RESULT_KEY="result";
@@ -15,9 +16,19 @@ public class DetailActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detail);
         Intent intent = getIntent();
-        Toast.makeText(this,intent.getStringExtra(PARAM_KEY),Toast.LENGTH_SHORT).show();
+        if (intent.getStringExtra(DetailActivity.PARAM_KEY)!=null) Toast.makeText(this,intent.getStringExtra(PARAM_KEY),Toast.LENGTH_SHORT).show();
+        setContentView(R.layout.activity_detail);
+        Button button = findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent1 = new Intent(DetailActivity.this, MasterActivity.class);
+                intent1.putExtra(DetailActivity.PARAM_KEY,"go back to single task master");
+                startActivity(intent1);
+            };
+        });
     }
 
     @Override
--- AndroidActivityTest/app/src/main/java/com/example/heping/testapp/MasterActivity.java
+++ AndroidActivityTest/app/src/main/java/com/example/heping/testapp/MasterActivity.java
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -17,8 +18,14 @@ public class MasterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_master);
+        Log.d("MasterActivity",this.toString());
+        Intent intent = getIntent();
+        if (intent.getStringExtra(DetailActivity.PARAM_KEY)!=null) Toast.makeText(this,intent.getStringExtra(DetailActivity.PARAM_KEY),Toast.LENGTH_SHORT).show();
         final Button btn1 =(Button)this.findViewById(R.id.button1);
         final Button bt2 = findViewById(R.id.button2);
+        final Button standardBtn = findViewById(R.id.StandardBtn);
+        final Button singleTopBtn = findViewById(R.id.SingleTopBtn);
+        final Button singleTaskBtn = findViewById(R.id.SingleTaskBtn);
         View.OnClickListener listener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -27,16 +34,48 @@ public class MasterActivity extends AppCompatActivity {
                 if (v.getId()==R.id.button1){
                     intent = new Intent(v.getContext(),DetailActivity.class);
                     intent.putExtra(DetailActivity.PARAM_KEY,"jump from an explicit intent");
+                    startActivityForResult(intent,REQUESTCODECODE_DETAIL);
                 }
                 else if (v.getId()==R.id.button2){
-                    intent = new Intent(DetailActivity.ACTION_DETAIL);
+                    intent = new Intent("com.example.heping.testapp.start_detail");
                     intent.putExtra(DetailActivity.PARAM_KEY,"jump from an implicit intent");
+                    startActivityForResult(intent,REQUESTCODECODE_DETAIL);
                 }
-                startActivityForResult(intent,REQUESTCODECODE_DETAIL);
+                else if (v.getId()==R.id.StandardBtn)
+                {
+                    intent = new Intent(MasterActivity.this,MasterActivity.class);
+                    startActivity(intent);
+                }
+                else if (v.getId() == R.id.SingleTopBtn)
+                {
+                    intent = new Intent(MasterActivity.this, DetailActivity.class);
+                    startActivity(intent);
+                }
+                else  if (v.getId() == R.id.SingleTaskBtn){
+                    intent = new Intent(MasterActivity.this, DetailActivity.class);
+                    startActivity(intent);
+                }
+
             }
         };
         btn1.setOnClickListener(listener);
         bt2.setOnClickListener(listener);
+        standardBtn.setOnClickListener(listener);
+        singleTopBtn.setOnClickListener(listener);
+        singleTaskBtn.setOnClickListener(listener);
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        setIntent(intent);
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Intent intent = getIntent();
+        if (intent.getStringExtra(DetailActivity.PARAM_KEY)!=null) Toast.makeText(this,intent.getStringExtra(DetailActivity.PARAM_KEY),Toast.LENGTH_SHORT).show();
     }
 
     @Override
