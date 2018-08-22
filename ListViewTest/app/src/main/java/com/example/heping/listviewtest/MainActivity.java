888ef35745854634ea1c252f4c070ae8f995affd
MemoryReload <heping_tsdwx@163.com>
2018-08-23 03:29:04 +0800

    some changes

--- ListViewTest/app/src/main/java/com/example/heping/listviewtest/MainActivity.java
+++ ListViewTest/app/src/main/java/com/example/heping/listviewtest/MainActivity.java
@@ -1,25 +1,88 @@
 package com.example.heping.listviewtest;
 
+import android.content.Context;
+import android.support.annotation.DrawableRes;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
-
-import java.lang.reflect.Array;
-
+import android.widget.TextView;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private String[] fruitNames= {"apple", "pear", "grape", "orange"};
-    private ListView fruitList;
+    private List<Fruit> fruits = new ArrayList<>();
+    private ListView fruitListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        this.fruitList = findViewById(R.id.fruitList);
-        ArrayAdapter<String> adapter = new ArrayAdapter(MainActivity.this, android.R.layout.simple_list_item_1, fruitNames);
-        this.fruitList.setAdapter(adapter);
+        this.fruitListView = findViewById(R.id.fruitList);
+        initFruits();
+        FruitAdapter adapter = new FruitAdapter(MainActivity.this, R.layout.list_item, fruits);
+        this.fruitListView.setAdapter(adapter);
+    }
+
+    protected void initFruits()
+    {
+        for (int i=0; i<30; i++)
+        {
+            Fruit fruit = new Fruit(R.drawable.pen,"Apple");
+            this.fruits.add(fruit);
+        }
+    }
+}
+
+class Fruit extends Object {
+
+    private int     mImageResource;
+    private String  mName;
+
+    public Fruit(@DrawableRes int image, String name)
+    {
+        this.mImageResource = image;
+        this.mName = name;
+    }
+
+    public String getName()
+    {
+        return this.mName;
+    }
+
+    public int getImage()
+    {
+        return this.mImageResource;
+    }
+}
+
+class FruitAdapter extends ArrayAdapter<Fruit>{
+
+    private int mItemResource;
+
+    public FruitAdapter(Context context, @LayoutRes int resource, List<Fruit> objects)
+    {
+        super(context, resource, objects);
+        this.mItemResource = resource;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View listItemView = LayoutInflater.from(getContext()).inflate(mItemResource,parent,false);
+        Fruit item = getItem(position);
+        ImageView iconView = listItemView.findViewById(R.id.icon);
+        TextView  text = listItemView.findViewById(R.id.text);
+        iconView.setImageResource(item.getImage());
+        text.setText(item.getName());
+        return listItemView;
     }
 }
