34b14b78e0d35b01807fbcad9a9d800700533747
heping <ping.he@chelaile.net.cn>
2018-09-06 18:26:09 +0800

    play with fragment

--- FragmentTest/app/src/main/java/com/example/heping/fragmenttest/MainActivity.java
+++ FragmentTest/app/src/main/java/com/example/heping/fragmenttest/MainActivity.java
@@ -2,12 +2,41 @@ package com.example.heping.fragmenttest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RadioGroup;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
 
+public class MainActivity extends AppCompatActivity implements RadioGroup.OnCheckedChangeListener {
+    private List<MyFragment> fragments;
+    private RadioGroup tabBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tabBar = findViewById(R.id.tab_bar);
+        initFragments();
+        tabBar.setOnCheckedChangeListener(this);
+        tabBar.check(R.id.item0);
+    }
+
+    private void initFragments(){
+        int count = tabBar.getChildCount();
+        fragments = new ArrayList<MyFragment>();
+        for (int i=0 ; i<count; i++){
+            MyFragment fragment = new MyFragment();
+            fragments.add(i,fragment);
+        }
+    }
+
+    @Override
+    public void onCheckedChanged(RadioGroup group, int checkedId) {
+        int index = this.tabBar.indexOfChild(this.tabBar.findViewById(checkedId));
+        showFragmentAtIndex(index);
+    }
+
+    private void showFragmentAtIndex(int index)
+    {
+
     }
 }
