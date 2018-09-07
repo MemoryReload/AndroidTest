a22c69a5afdec44bc45c14aee3e5ddacf00dc65b
heping <ping.he@chelaile.net.cn>
2018-09-07 15:46:57 +0800

    activity向fragment传递参数，改变背景色

--- FragmentTest/app/src/main/java/com/example/heping/fragmenttest/MainActivity.java
+++ FragmentTest/app/src/main/java/com/example/heping/fragmenttest/MainActivity.java
@@ -10,27 +10,27 @@ import android.view.View;
 import android.widget.RadioGroup;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements RadioGroup.OnCheckedChangeListener {
-    private List<MyFragment> fragments;
+    private ArrayList<Fragment> fragments;
+    private Fragment currentFragment;
     private RadioGroup tabBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         tabBar = findViewById(R.id.tab_bar);
-        initFragments();
         tabBar.setOnCheckedChangeListener(this);
+        initFragments();
         tabBar.check(R.id.item0);
     }
 
-    private void initFragments(){
-        int count = tabBar.getChildCount();
-        fragments = new ArrayList<MyFragment>();
-        for (int i=0 ; i<count; i++){
-            MyFragment fragment = new MyFragment();
-            fragments.add(i,fragment);
+    private void initFragments()
+    {
+        currentFragment = null;
+        fragments = new ArrayList<Fragment>(tabBar.getChildCount());
+        for (int i=0; i<tabBar.getChildCount(); i++){
+            fragments.add(null);
         }
     }
 
@@ -42,16 +42,22 @@ public class MainActivity extends AppCompatActivity implements RadioGroup.OnChec
 
     private void showFragmentAtIndex(int index)
     {
-        Fragment newFragment = fragments.get(index);
         FragmentManager manager = getSupportFragmentManager();
+        Fragment fragmentToShow = fragments.get(index);
         FragmentTransaction transaction = manager.beginTransaction();
-        transaction.replace(R.id.fragment, newFragment);
-        transaction.commit();
-
-        String[] colors = {"#FF0000","#00FF00","#0000FF","#FF5A00"};
-        View view= newFragment.getView();
-        String color = colors[colors.length%4];
-        int c = Color.parseColor(color);
-        view.setBackgroundColor(c);
+        if (currentFragment != null)
+        {
+            transaction.hide(currentFragment);
+        }
+        if(fragmentToShow == null) {
+            fragmentToShow = new MyFragment();
+            String[] colors = {"#FF0000","#00FF00","#0000FF","#FF5A00"};
+            String color = colors[index%colors.length];
+            Bundle bundle = new Bundle();
+            bundle.putString("color",color);
+            fragmentToShow.setArguments(bundle);
+            transaction.add(R.id.fragment, fragmentToShow);
+        }
+        transaction.show(fragmentToShow).commit();
     }
 }
--- FragmentTest/app/src/main/java/com/example/heping/fragmenttest/MyFragment.java
+++ FragmentTest/app/src/main/java/com/example/heping/fragmenttest/MyFragment.java
@@ -1,5 +1,6 @@
 package com.example.heping.fragmenttest;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -13,6 +14,8 @@ public class MyFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View myView = inflater.inflate(R.layout.fragment,container,false);
+        String color = getArguments().getString("color");
+        myView.setBackgroundColor(Color.parseColor(color));
         return myView;
     }
 }
