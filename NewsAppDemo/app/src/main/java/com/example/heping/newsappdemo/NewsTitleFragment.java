d713a667e07692bed5d76d8105bc11c6ca6ca040
heping <ping.he@chelaile.net.cn>
2018-10-10 19:33:37 +0800

    ssss

--- NewsAppDemo/app/src/main/java/com/example/heping/newsappdemo/NewsTitleFragment.java
+++ NewsAppDemo/app/src/main/java/com/example/heping/newsappdemo/NewsTitleFragment.java
@@ -4,19 +4,83 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
 
 public class NewsTitleFragment extends Fragment {
+    public boolean isTowPane;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.new_title_frag,container,false);
+        return  view;
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        if (getActivity().findViewById(R.id.news_content_layout) != null){
+            isTowPane = true;
+        }
+        else {
+            isTowPane = false;
+        }
+    }
+
+    class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.ViewHolder>
+    {
+        private List<News> mNewsList;
+
+        void NewsAdapter(List<News> news)
+        {
+            mNewsList = news;
+        }
+
+        class ViewHolder extends RecyclerView.ViewHolder
+        {
+            public TextView titleView;
+
+            public ViewHolder(@NonNull View itemView) {
+                super(itemView);
+                titleView = itemView.findViewById(R.id.news_title);
+            }
+        }
+
+        @NonNull
+        @Override
+        public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+            View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.news_item,viewGroup,false);
+            final  ViewHolder holder = new ViewHolder(view);
+            view.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    News news = mNewsList.get(holder.getAdapterPosition());
+                    if (isTowPane == true){
+                        NewsContentFragment contentFragment = (NewsContentFragment) getActivity().getSupportFragmentManager().findFragmentById(R.id.news_content_fragment);
+                        contentFragment.refresh(news.getTitle(),news.getContent());
+                    }
+                    else {
+                        NewsContentActivity.startAction(getActivity(),news.getTitle(),news.getContent());
+                    }
+                }
+            });
+            return holder;
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+            News item = mNewsList.get(i);
+            viewHolder.titleView.setText(item.getTitle());
+        }
+
+        @Override
+        public int getItemCount() {
+            return mNewsList.size();
+        }
     }
 }
