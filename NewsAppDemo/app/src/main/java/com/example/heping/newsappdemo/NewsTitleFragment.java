4f9c3a19b88f89800e2578be14461231be75690d
MemoryReload <heping_tsdwx@163.com>
2018-10-11 02:07:35 +0800

    something more

--- NewsAppDemo/app/src/main/java/com/example/heping/newsappdemo/NewsTitleFragment.java
+++ NewsAppDemo/app/src/main/java/com/example/heping/newsappdemo/NewsTitleFragment.java
@@ -4,13 +4,16 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class NewsTitleFragment extends Fragment {
     public boolean isTowPane;
@@ -18,9 +21,36 @@ public class NewsTitleFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.new_title_frag,container,false);
+        RecyclerView recyclerView = view.findViewById(R.id.news_title_recycler_view);
+        NewsAdapter adapter = new NewsAdapter(getNews());
+        LinearLayoutManager manager = new LinearLayoutManager(getActivity());
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(manager);
         return  view;
     }
+    
+    private List<News> getNews()
+    {
+        List<News> newsList = new ArrayList<News>();
+        for (int i = 0; i < 50; i++) {
+            News news = new News();
+            news.setTitle("This is news title"+i);
+            news.setContent(getRandomLengthContent("This is news content"+i+"."));
+            newsList.add(news);
+        }
+        return newsList;
+    }
 
+    private String getRandomLengthContent(String content)
+    {
+        Random random = new Random();
+        int length = random.nextInt(20)+1;
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            builder.append(content);
+        }
+        return builder.toString();
+    }
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
@@ -36,7 +66,7 @@ public class NewsTitleFragment extends Fragment {
     {
         private List<News> mNewsList;
 
-        void NewsAdapter(List<News> news)
+        public NewsAdapter(List<News> news)
         {
             mNewsList = news;
         }
