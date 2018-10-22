c728566d99ca70d6db61d8e6863490276805d906
heping <ping.he@chelaile.net.cn>
2018-10-22 11:14:25 +0800

    fix some stupid bugs

--- BroadcastTest/app/src/main/java/com/example/heping/broadcasttest/MyApplication.java
+++ BroadcastTest/app/src/main/java/com/example/heping/broadcasttest/MyApplication.java
@@ -7,12 +7,12 @@ public class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        Toast.makeText(this,"Application created, babe!",Toast.LENGTH_SHORT);
+        Toast.makeText(this,"Application created, babe!",Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void onTerminate() {
         super.onTerminate();
-        Toast.makeText(this,"Application terminated, babe!",Toast.LENGTH_SHORT);
+        Toast.makeText(this,"Application terminated, babe!",Toast.LENGTH_SHORT).show();
     }
 }
